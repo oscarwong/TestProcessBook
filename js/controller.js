118626d6838b2d10c2b847ced64e964b5d9f16c6
Oscar Wong <oscarwong@Oscar-Wongs-MacBook-Pro.local>
2013-12-12 04:15:54 -0800

    Finished

--- js/controller.js
+++ js/controller.js
@@ -1,19 +1,3 @@
 $(document).ready(function() {
-	$('div.links a').smoothScroll();
 
-	$('#change-speed').on('click', function() {
-		var $p1 = $('div.chapters a').first(),
-		    p1Opts = $p1.smoothScroll('options') || {};
-
-		p1Opts.speed = p1Opts.speed === 1400 ? 400 : 1400;
-		$p1.smoothScroll('options', p1Opts);
-	});
-})
-
-// $('button.scrollsomething').on('click', function() {
-// 	$.smoothScroll({
-// 		scrollElement: $('div.intro'),
-// 		scrollTarget: $('div.introduction')
-// 	});
-// 	return false;
-// })
+}) //document ready
